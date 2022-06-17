6071ce8067a6a4fb989236e21ffe54fc2f766fff
ndm7kor <105267513+ndm7kor@users.noreply.github.com>
2022-06-17 16:39:21 +0530

    Create TestColourCode.c

---
+++ TestColourCode.c
@@ -0,0 +1,22 @@
+#include "ColourCode.h"
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    for (int i=1;i<=25;i++) {
+    printf("Got pair %s\n", colorPairNames);}
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
