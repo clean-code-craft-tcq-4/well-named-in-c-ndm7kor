f9c6e0c3b0010c7f2aee9dd75f6c6f34654aa4fe
ndm7kor <105267513+ndm7kor@users.noreply.github.com>
2022-06-17 16:37:21 +0530

    Create GetColourCode.c

---
+++ GetColourCode.c
@@ -0,0 +1,23 @@
+#include "Color_Code.h"
+
+const char* majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int numberOfMajorColors = sizeof(majorColors) / sizeof(majorColors[0]);
+int numberOfMinorColors = sizeof(minorColors) / sizeof(minorColors[0]);
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) 
+{
+    sprintf(buffer, "%s %s",majorColors[colorPair->majorColor],minorColors[colorPair->minorColor]);
+}
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
+}
