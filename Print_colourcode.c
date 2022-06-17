27c4023c9b867ac4fa4f63ebca6bd309b161e00d
ndm7kor <105267513+ndm7kor@users.noreply.github.com>
2022-06-17 08:14:57 +0530

    Create Print_colourcode.c

---
+++ Print_colourcode.c
@@ -0,0 +1,17 @@
+
+void PrintColorCode_pairnumber()
+{
+  int major_colour;
+  int minor_colour;
+  int pairnumber = 1;
+  
+  Printf("Pair Number. Manjor colur Minor colour \n");
+  for(major_colour = 0; major_colour < numberOfMajorColors; major_colour++)
+    {
+        for(minor_colour = 0; minor_colour < numberOfMinorColors; minor_colour++)
+        {
+            printf("   %d \t    %s\t  %s\t\n",pairnumber++, majorColors[major_colour], minorColors[major_colour]);
+        }
+    }
+  
+}
